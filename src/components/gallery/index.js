041f7b0d682ae2ae09ce87beb28800ef2027181a
sameer-manek <sameermanek@hotmail.com>
2020-02-24 10:56:31 +0530

    dev - added lazy loading and suspense

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,34 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense } from 'react'
 import './gallery.css'
+import Spinner from '../spinner'
+
+const Thumbnail = React.lazy(() => import('../thumbnail'))
 
 const styles = {
     wrapper: {
         display: "flex",
         width: "90%",
-        maxWidth: "1200px",
+        maxWidth: "960px",
         margin: "10px auto",
 	}
 }
 
 class Gallery extends Component
 {
-    state = {
-		cols: 3
+	constructor()
+	{
+		super()
+
+		this.state = {
+			cols: 3,
+			thumbnails: 30
+		}
+
+		this.handleWindowResize = this.handleWindowResize.bind(this)
 	}
 
 	handleWindowResize()
 	{
-		console.log("window resized")
+		if (window.innerWidth >= 960) // tablet
+			this.setState({
+				cols: 3
+			})
+		else if (window.innerWidth < 960 && window.innerWidth > 480) // tablet
+			this.setState({
+				cols: 2
+			})
+		else
+			this.setState({
+				cols: 1
+			})
 	}
 
-	onComponentDidMount()
+	componentDidMount()
 	{
-		window.addEventListener("resize", this.handleWindowResize);
+		this.handleWindowResize();
+		window.addEventListener("resize", this.handleWindowResize)
 	}
 
-	onComponentWillUnmount()
+	componentWillUnmount()
 	{
-		window.removeEventListener("resize", this.handleWindowResize);
+		console.log("component is unmounting")
+		window.removeEventListener("resize", this.handleWindowResize)
 	}
 	 
 	createGrid ()
@@ -37,12 +61,12 @@ class Gallery extends Component
 		for (let i = 0; i < this.state.cols; i++)
 		{
 			let col = [];
-			for (let n = 0; n < 5; n++)
+			for (let n = 0; n < this.state.thumbnails / this.state.cols; n++)
 			{
 				let key = (''+i)+(''+n)
-				col.push(<img src="https://picsum.photos/id/242/240/300" alt="img" className="img" key={key} />)
+				col.push(<Suspense fallback={<Spinner />}> <Thumbnail /> </Suspense>)
 			}
-			grid.push(<div className="column" key={''+i}> {col} </div>);
+			grid.push(<div className="column" key={''+i}> {col} </div>)
 		}
 
 		return grid;
@@ -58,4 +82,4 @@ class Gallery extends Component
     }
 }
 
-export default Gallery;
+export default Gallery
---
+++ src/components/thumbnail/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from  'react'
+
+class Thumbnail extends Component
+{
+    render()
+    {
+        return (<img src="https://picsum.photos/id/242/240/300" alt="img" className="img" />)
+        
+    }
+}
+
+export default Thumbnail
